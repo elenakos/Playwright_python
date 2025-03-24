8ecee8558f813de5d4a05537477abd48a150f84a
elenakos <kostrygina@gmail.com>
2025-03-24 11:46:30 -0700

    Updated README.md

---
+++ Tests/test_verify_page_elements.py
@@ -0,0 +1,20 @@
+import re
+from playwright.sync_api import Page, expect
+
+# Variables
+URL = "https://playwright.dev/"
+PAGE_TITLE = "Playwright"
+LINK_NAME = "Get started"
+HEADING_NAME = "Installation"
+
+def test_has_title(page: Page):
+    page.goto(URL)
+    # Expect a title "to contain" a substring.
+    expect(page).to_have_title(re.compile(PAGE_TITLE))
+
+def test_get_started_link(page: Page):
+    page.goto(URL)
+    # Click the get started link.
+    page.get_by_role("link", name=LINK_NAME).click()
+    # Expects page to have a heading with the name of Installation.
+    expect(page.get_by_role("heading", name=HEADING_NAME)).to_be_visible()
