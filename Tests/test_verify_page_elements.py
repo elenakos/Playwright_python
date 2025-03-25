c3b6eea537aedc817ffa7a8f7bba7c7ec529b8b6
elenakos <kostrygina@gmail.com>
2025-03-24 17:46:35 -0700

    Updated test cases

--- Tests/test_verify_page_elements.py
+++ Tests/test_verify_page_elements.py
@@ -1,20 +1,33 @@
 import re
 from playwright.sync_api import Page, expect
+import logging
 
 # Variables
 URL = "https://playwright.dev/"
 PAGE_TITLE = "Playwright"
 LINK_NAME = "Get started"
 HEADING_NAME = "Installation"
+LINK_COMMUNITY = "Community"
+HEADING_WELCOME = "Welcome"
+LINK_AMBASSADOR = "Ambassador"
+AMBASSADOR_IMAGE_TEXT = "Are you the next Ambassador?'"
 
 def test_has_title(page: Page):
     page.goto(URL)
-    # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile(PAGE_TITLE))
 
-def test_verify_get_started_link(page: Page):
+def test_verify_link_is_displayed(page: Page):
     page.goto(URL)
-    # Click the get started link.
     page.get_by_role("link", name=LINK_NAME).click()
-    # Expects page to have a heading with the name of Installation.
     expect(page.get_by_role("heading", name=HEADING_NAME)).to_be_visible()
+
+def test_navigate_to_community_page(page: Page):
+    page.goto(URL)
+    page.get_by_role("link", name=LINK_COMMUNITY).click()
+    expect(page.get_by_role("heading", name=HEADING_WELCOME)).to_be_visible()
+    page.get_by_role("link", name=LINK_AMBASSADOR).first.click()
+    image_next_ambassador = page.get_by_role("img", name=AMBASSADOR_IMAGE_TEXT)
+    expect(image_next_ambassador).to_be_visible()
+
+
+
